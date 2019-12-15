558d5df229e9da1eed3b09b4b5b05624392aff07
Domantas <domantasgiedraitis@gmail.com>
2019-12-15 11:56:45 +0200

    CORS modification

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
